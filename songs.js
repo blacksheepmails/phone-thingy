e6caced2f8953523820c8081bf7093f5909fb1bb
Maslab eeePC <blacksheepmails@gmail.com>
2015-05-26 17:51:20 +0100

    put json in separate file

---
+++ songs.js
@@ -0,0 +1,4 @@
+var song = {"title":"Song Name",
+            "artist":"Some Band",
+            "album":"Albums are still a new concept",
+            "lyrics":"Maybe you can swing by my room around 10\nBaby bring the lemon and a bottle of gin"}
