466b46820055a9b2d23a571dbd98f19a6dfa9d88
Maslab eeePC <blacksheepmails@gmail.com>
2015-05-27 01:16:25 +0100

    added main function in javascript

--- musicapp.js
+++ musicapp.js
@@ -1,36 +1,41 @@
+function main () {
+    populateSongFields(song);
 
-var lyrics = song.lyrics.split("\n");
-document.getElementById("song-info").innerHTML = song.artist + ", " + song.title;
-document.getElementById("lyrics-current").innerHTML = lyrics[0];
-document.getElementById("lyrics-next").innerHTML = lyrics[1];
+    var down = false;
 
-var down = false;
-
-document.getElementById('progress').addEventListener("mousedown", function(e) {
-    down = true;
-    moveSlider(e);
-    });
-document.addEventListener("mousemove", function(e) {
-    if (down) {
+    document.getElementById('progress').addEventListener("mousedown", function(e) {
+        down = true;
         moveSlider(e);
-        }
-    });
-document.addEventListener("mouseup", function(e) {
-    down = false;
-    } );
+        });
+    document.addEventListener("mousemove", function(e) {
+        if (down) {
+            moveSlider(e);
+            }
+        });
+    document.addEventListener("mouseup", function(e) {
+        down = false;
+        } );
 
-document.getElementById('progress').addEventListener("touchstart", function(e) {
-    down = true;
-    moveSlider(e);
-    });
-document.addEventListener("touchmove", function(e) {
-    if (down) {
+    document.getElementById('progress').addEventListener("touchstart", function(e) {
+        down = true;
         moveSlider(e);
-        }
-    });
-document.addEventListener("touchend", function(e) {
-    down = false;
-    } );
+        });
+    document.addEventListener("touchmove", function(e) {
+        if (down) {
+            moveSlider(e);
+            }
+        });
+    document.addEventListener("touchend", function(e) {
+        down = false;
+        } );
+    }
+
+function populateSongFields (song) {
+    var lyrics = song.lyrics.split("\n");
+    document.getElementById("song-info").innerHTML = song.artist + ", " + song.title;
+    document.getElementById("lyrics-current").innerHTML = lyrics[0];
+    document.getElementById("lyrics-next").innerHTML = lyrics[1];
+}
 
 function moveSlider (event) {
     var newProgress = event.pageX;
